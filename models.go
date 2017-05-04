59f820a264af2ec0f1dbd8a4970f77e83cd62b36
akuma06 <demon.akuma06@gmail.com>
2017-05-04 17:16:20 +0200

    Updated Models.go (simplified + orderBy param)
    Added a new main function to get Torrents with orderBy parameter
    Added a new function to get all torrents with orderBy parameter
    Simplifying the other function that get torrents so they pass through one main function that manage db requests.
    Commented AutoMigrate functions, it emits warning when you execute it after having migrated once

--- models.go
+++ models.go
@@ -13,13 +13,13 @@ type Categories struct {
 	Category_id   int
 	Category_name string
 	Torrents      []Torrents       `gorm:"ForeignKey:category_id;AssociationForeignKey:category_id"`
-	Sub_category  []Sub_Categories `gorm:"ForeignKey:category_id;AssociationForeignKey:parent_id"`
+	Sub_Categories  []Sub_Categories `gorm:"ForeignKey:category_id;AssociationForeignKey:parent_id"`
 }
 
 type Sub_Categories struct {
-	Sub_category_id   int
-	Sub_category_name string
-	Parent_id         int
+	Sub_category_id   int          `gorm:"column:sub_category_id"`
+	Sub_category_name string       `gorm:"column:Sub_category_name"`
+	Parent_id         int          `gorm:"column:parent_id"`
 	Torrents          []Torrents `gorm:"ForeignKey:sub_category_id;AssociationForeignKey:sub_category_id"`
 }
 
@@ -89,32 +89,51 @@ func getTorrentById(id string) (Torrents, error) {
 	return torrent, nil
 }
 
-func getTorrents(parameters WhereParams, limit int, offset int) []Torrents {
+func getTorrentsOrderBy(parameters *WhereParams, orderBy string, limit int, offset int) []Torrents {
 	var torrents []Torrents
-	db.Limit(limit).Offset(offset).Order("torrent_id DESC").Where(parameters.conditions, parameters.params...).Preload("Categories").Preload("Sub_Categories").Find(&torrents)
+	var dbQuery *gorm.DB
+	
+	if (parameters != nil) {  // if there is where parameters
+		dbQuery = db.Model(&torrents).Where(parameters.conditions, parameters.params...)
+	} else {
+		dbQuery = db.Model(&torrents)
+	} 
+	
+	if (orderBy == "") { orderBy = "torrent_id DESC" } // Default OrderBy
+	if limit != 0 || offset != 0 { // if limits provided
+		dbQuery = dbQuery.Limit(limit).Offset(offset)
+	}
+		dbQuery.Order(orderBy).Preload("Categories").Preload("Sub_Categories").Find(&torrents)
 	return torrents
 }
 
+/* Functions to simplify the get parameters of the main function 
+ * 
+ * Get Torrents with where parameters and limits, order by default
+ */
+func getTorrents(parameters WhereParams, limit int, offset int) []Torrents {
+	return getTorrentsOrderBy(&parameters, "", limit, offset)
+}
+
+/* Get Torrents with where parameters but no limit and order by default (get all the torrents corresponding in the db)
+ */
 func getTorrentsDB(parameters WhereParams) []Torrents {
-	var torrents []Torrents
-	db.Where(parameters.conditions, parameters.params...).Order("torrent_id DESC").Preload("Categories").Preload("Sub_Categories").Find(&torrents)
-	return torrents
+	return getTorrentsOrderBy(&parameters, "", 0, 0)
 }
 
 /* Function to get all torrents
  */
 
-func getAllTorrents(limit int, offset int) []Torrents {
-	var torrents []Torrents
-	db.Model(&torrents).Limit(limit).Offset(offset).Order("torrent_id DESC").Preload("Categories").Preload("Sub_Categories").Find(&torrents)
+func getAllTorrentsOrderBy(orderBy string, limit int, offset int) [] Torrents {
+	return getTorrentsOrderBy(nil, orderBy, limit, offset)
+}
 
-	return torrents
+func getAllTorrents(limit int, offset int) []Torrents {
+	return getTorrentsOrderBy(nil, "", limit, offset)
 }
 
 func getAllTorrentsDB() []Torrents {
-	var torrents []Torrents
-	db.Order("torrent_id DESC").Preload("Categories").Preload("Sub_Categories").Find(&torrents)
-	return torrents
+	return getTorrentsOrderBy(nil, "", 0, 0)
 }
 
 /* Function to get all categories with/without torrents (bool)
