005785f4f8740391a070ea1deb4bead682ec7fd9
bakape <scorpid33@gmail.com>
2017-05-06 20:01:15 +0300

    Don't reparse templates on every fucking request

--- router/faqHandler.go
+++ router/faqHandler.go
@@ -1,19 +1,24 @@
 package router
 
-
-import(
-	"net/http"
+import (
 	"html/template"
+	"net/http"
+
 	"github.com/gorilla/mux"
 )
 
+var faqTemplate = template.Must(template.New("FAQ").Funcs(FuncMap).ParseFiles("templates/index.html", "templates/FAQ.html"))
+
+func init() {
+	// common
+	template.Must(faqTemplate.ParseGlob("templates/_*.html"))
+}
+
 func FaqHandler(w http.ResponseWriter, r *http.Request) {
-	var templates = template.Must(template.New("FAQ").Funcs(FuncMap).ParseFiles("templates/index.html", "templates/FAQ.html"))
- 	templates.ParseGlob("templates/_*.html") // common
 	searchForm := NewSearchForm()
- 	searchForm.HideAdvancedSearch = true
- 	err := templates.ExecuteTemplate(w, "index.html", FaqTemplateVariables{Navigation{}, searchForm, r.URL, mux.CurrentRoute(r)})
- 		if err != nil {
+	searchForm.HideAdvancedSearch = true
+	err := faqTemplate.ExecuteTemplate(w, "index.html", FaqTemplateVariables{Navigation{}, searchForm, r.URL, mux.CurrentRoute(r)})
+	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
--- router/homeHandler.go
+++ router/homeHandler.go
@@ -1,19 +1,24 @@
 package router
 
-import(
-	"net/http"
-	"html/template"
-	"github.com/gorilla/mux"
+import (
 	"html"
+	"html/template"
+	"net/http"
 	"strconv"
+
 	"github.com/ewhal/nyaa/model"
 	"github.com/ewhal/nyaa/service/torrent"
+	"github.com/gorilla/mux"
 )
 
+var homeTemplate = template.Must(template.New("home").Funcs(FuncMap).ParseFiles("templates/index.html", "templates/home.html"))
+
+func init() {
+	template.Must(homeTemplate.ParseGlob("templates/_*.html")) // common
+}
+
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-var templates = template.Must(template.New("home").Funcs(FuncMap).ParseFiles("templates/index.html", "templates/home.html"))
-	templates.ParseGlob("templates/_*.html") // common
- 	vars := mux.Vars(r)
+	vars := mux.Vars(r)
 	page := vars["page"]
 
 	// db params url
@@ -39,7 +44,7 @@ var templates = template.Must(template.New("home").Funcs(FuncMap).ParseFiles("te
 	navigationTorrents := Navigation{nbTorrents, maxPerPage, pagenum, "search_page"}
 	htv := HomeTemplateVariables{b, torrentService.GetAllCategories(false), NewSearchForm(), navigationTorrents, r.URL, mux.CurrentRoute(r)}
 
-	err := templates.ExecuteTemplate(w, "index.html", htv)
+	err := homeTemplate.ExecuteTemplate(w, "index.html", htv)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
