ded00e5cebc006305aa9339889bbc2c610e26624
tomleb <tomlebreux@hotmail.com>
2017-05-06 01:32:43 -0400

    Add postgres docker support
    Not tested yet as the current postgres dump is incompatible with the
    application. We need to fix ourselve to one schema and stick to it for
    whatever database backend we support.

--- deploy/restore.sh
+++ deploy/restore.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+# Restore the database from a postgres dump
 
-psql -v ON_ERROR_STOP=1 --username "${POSTGRES_USER}" < "${PANTSU_POSTGRES_DBFILE}"
+pg_restore --username "${POSTGRES_USER}" -d ${POSTGRES_DB} "/${PANTSU_POSTGRES_DBFILE}"
