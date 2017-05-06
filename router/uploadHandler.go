e82e97b961db19aeee8e9f44e63158a1ae1333b6
bakape <scorpid33@gmail.com>
2017-05-07 02:20:13 +0300

    Revert quicktemplate search template

--- router/uploadHandler.go
+++ router/uploadHandler.go
@@ -4,11 +4,14 @@ import (
 	"html/template"
 	"net/http"
 
-	"github.com/ewhal/nyaa/templates"
 	"github.com/gorilla/mux"
 )
 
 var uploadTemplate = template.Must(template.New("upload").Funcs(FuncMap).ParseFiles("templates/index.html", "templates/upload.html"))
+
+func init() {
+	template.Must(uploadTemplate.ParseGlob("templates/_*.html")) // common
+}
 
 func UploadHandler(w http.ResponseWriter, r *http.Request) {
 	var err error
@@ -21,7 +24,7 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 			//add to db and redirect depending on result
 		}
 	} else if r.Method == "GET" {
-		htv := UploadTemplateVariables{uploadForm, templates.NewSearchForm(), templates.Navigation{}, r.URL, mux.CurrentRoute(r)}
+		htv := UploadTemplateVariables{uploadForm, NewSearchForm(), Navigation{}, r.URL, mux.CurrentRoute(r)}
 		err = uploadTemplate.ExecuteTemplate(w, "index.html", htv)
 	} else {
 		w.WriteHeader(http.StatusMethodNotAllowed)
