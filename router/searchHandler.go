28ef70c960cb2297cec219b36d6918c0eb21fbdf
Jeff Becker <ampernand@gmail.com>
2017-05-07 13:50:55 -0400

    make rss feeds faster

--- router/searchHandler.go
+++ router/searchHandler.go
@@ -23,7 +23,7 @@ func SearchHandler(w http.ResponseWriter, r *http.Request) {
 
 	search_param, torrents, nbTorrents := search.SearchByQuery(r, pagenum)
 
-	for i, _ := range torrents {
+	for i := range torrents {
 		res := torrents[i].ToJson()
 		b = append(b, res)
 	}
