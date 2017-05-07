04bd6ff4c55ff3001d1ce9b479d28ae39cdb58e8
Andrew Zhao <azhao@ugcs.caltech.edu>
2017-05-06 17:48:24 -0700

    get the proper magnet and infohash for upload

--- util/magnet.go
+++ util/magnet.go
@@ -1,15 +1,13 @@
 package util
 
 import (
-	"encoding/hex"
 	"fmt"
 	"net/url"
 )
 
 // convert a binary infohash to a magnet uri given a display name and tracker urls
-func InfoHashToMagnet(ih [20]byte, name string, trackers ...url.URL) (str string) {
-	str = hex.EncodeToString(ih[:])
-	str = fmt.Sprintf("magnet:?xt=urn:btih:%s", str)
+func InfoHashToMagnet(ih string, name string, trackers ...url.URL) (str string) {
+	str = fmt.Sprintf("magnet:?xt=urn:btih:%s", ih)
 	if len(name) > 0 {
 		str += fmt.Sprintf("&dn=%s", name)
 	}
