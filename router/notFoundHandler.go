cfaf54052a6ef679b0020077a1303a1c8cf26a26
Andrew Zhao <azhao@ugcs.caltech.edu>
2017-05-06 23:59:45 -0700

    added custom 404 page

---
+++ router/notFoundHandler.go
@@ -0,0 +1,24 @@
+package router
+
+import (
+	"html/template"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+var notFoundTemplate = template.Must(template.New("NotFound").Funcs(FuncMap).ParseFiles("templates/index.html", "templates/404.html"))
+
+func init() {
+	// common
+	template.Must(notFoundTemplate.ParseGlob("templates/_*.html"))
+}
+
+func NotFoundHandler(w http.ResponseWriter, r *http.Request) {
+	searchForm := NewSearchForm()
+	searchForm.HideAdvancedSearch = true
+	err := notFoundTemplate.ExecuteTemplate(w, "index.html", NotFoundTemplateVariables{Navigation{}, searchForm, r.URL, mux.CurrentRoute(r)})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
