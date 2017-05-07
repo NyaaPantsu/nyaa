37bd6e70d645950229d26010a99115d6aabb1962
ayame-git <s.varezhkin.601@gmail.com>
2017-05-07 17:08:45 +0300

    post handle now checks for t and c

--- service/user/form/formValidator.go
+++ service/user/form/formValidator.go
@@ -38,6 +38,12 @@ func ValidateUsername(username string) bool {
 	}
 	return true
 }
+func IsAgreed(t_and_c string) bool {
+	if t_and_c == "1" {
+		return true
+	}
+	return false
+}
 
 // RegistrationForm is used when creating a user.
 type RegistrationForm struct {
