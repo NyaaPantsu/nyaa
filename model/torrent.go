f9884c40e90dd8eb5419f7f3e361fba1654281cf
Andrew Zhao <azhao@ugcs.caltech.edu>
2017-05-06 22:18:41 -0700

    throw in the description with some sanitizatoin

--- model/torrent.go
+++ model/torrent.go
@@ -30,7 +30,7 @@ type Torrents struct {
 	Date            int64          `gorm:"column:date"`
 	Downloads       int            `gorm:"column:downloads"`
 	Filesize        int64          `gorm:"column:filesize"`
-	Description     []byte         `gorm:"column:description"`
+	Description     string         `gorm:"column:description"`
 	Comments        []byte         `gorm:"column:comments"`
 }
 
