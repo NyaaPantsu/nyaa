173dc9e34c98cbd49634c2d0cd9bc16b9ef0dcd2
sfan5 <sfan5@live.de>
2017-05-08 20:21:11 +0200

    Remove unused code

--- db/gorm.go
+++ db/gorm.go
@@ -30,7 +30,6 @@ func GormInit(conf *config.Config) (*gorm.DB, error) {
 	// db.SingularTable(true)
 	if config.Environment == "DEVELOPMENT" {
 		db.LogMode(true)
-		// db.DropTable(&model.User{}, "UserFollower")
 		db.AutoMigrate(&model.User{}, &model.UserFollows{})
 		db.AutoMigrate(&model.User{}, &model.Torrents{}, &model.Comment{}, &model.OldComment{})
 		// db.Model(&model.User{}).AddIndex("idx_user_token", "token")
