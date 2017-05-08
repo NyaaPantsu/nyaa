263b1f5075b875e48974efe03bcea90f9525b441
Andrew Zhao <azhao@ugcs.caltech.edu>
2017-05-07 19:06:11 -0700

    implemented comments and display

--- model/comment.go
+++ model/comment.go
@@ -6,13 +6,14 @@ import (
 
 // Comment is a comment model.
 type Comment struct {
-	Id          int      `json:"id"`
-	Content     string    `json:"content"`
-	UserId      int      `json:"userId"`
-	TorrentId	int
+	Id        int    `json:"id"`
+	Content   string `json:"content"`
+	UserId    int    `json:"userId"`
+	Username  string `json:"username"` // this is duplicate but it'll be faster rite?
+	TorrentId int
 	// LikingCount int       `json:"likingCount"`
-	CreatedAt   time.Time `json:"createdAt"`
-	UpdatedAt   time.Time `json:"updatedAt"`
-	DeletedAt   time.Time `json:"deletedAt"`
-	User        User      `json:"user"`
+	CreatedAt time.Time `json:"createdAt"`
+	UpdatedAt time.Time `json:"updatedAt"`
+	DeletedAt time.Time `json:"deletedAt"`
+	User      User      `json:"user"`
 }
