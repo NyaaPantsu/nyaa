dff098da6ab1d04ac45c132917bbe20ce7d7d2b3
tomleb <tomlebreux@hotmail.com>
2017-05-08 01:20:51 -0400

    Create .torrents file from .xz and .sig

--- deploy/ansible/roles/backup/files/backup.sh
+++ deploy/ansible/roles/backup/files/backup.sh
@@ -7,6 +7,7 @@ NYAAPANTSU_USERNAME="$1"
 NYAAPANTSU_PASSWORD="$2"
 NYAAPANTSU_DB="$3"
 NYAAPANTSU_PASSPHRASE_FILE="$4"
+NYAAPANTSU_TRACKER="$5"
 
 dump_file="${NYAAPANTSU_DB}_$(date +'%Y_%m_%d_%H_%M').backup"
 
@@ -15,7 +16,10 @@ pg_dump -U "${NYAAPANTSU_USERNAME}" -f "${dump_file}"
 xz -z "${dump_file}"
 
 compressed_dump_file="${dump_file}.xz"
+signature_file="${compressed_dump_file}.sig"
 
 gpg2 --batch --yes --passphrase-fd 0 \
-     --output "${compressed_dump_file}.sig" \
+     --output "${signature_file}" \
      --detach-sig "${compressed_dump_file}" < "${NYAAPANTSU_PASSPHRASE_FILE}"
+
+mktorrent -a "${NYAAPANTSU_TRACKER}" "${compressed_dump_file}" "${signature_file}"
