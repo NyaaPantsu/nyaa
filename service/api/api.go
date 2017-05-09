59ed2ec4c6d0c3b0893f4adfb5c954a16e720f4c
ayame-git <s.varezhkin.601@gmail.com>
2017-05-09 21:54:50 +0300

    update

--- service/api/api.go
+++ service/api/api.go
@@ -8,6 +8,7 @@ import (
 	"regexp"
 	"strings"
 
+	"github.com/ewhal/nyaa/model"
 	"github.com/ewhal/nyaa/service/torrent"
 )
 
@@ -26,13 +27,18 @@ type TorrentsRequest struct {
 }
 
 //accept torrent files?
-type UploadRequest struct {
+type TorrentRequest struct {
 	Name        string `json:"name"`
 	Hash        string `json:"hash"`
 	Magnet      string `json:"magnet"`
 	Category    int    `json:"category"`
 	SubCategory int    `json:"sub_category"`
 	Description string `json:"description"`
+}
+
+type UpdateRequest struct {
+	Id     int            `json:"id"`
+	Update TorrentRequest `json:"update"`
 }
 
 func (r *TorrentsRequest) ToParams() torrentService.WhereParams {
@@ -60,7 +66,9 @@ var ErrSubCategory = errors.New("this sub category doesn't exist")
 var ErrMagnet = errors.New("incorrect magnet")
 var ErrHash = errors.New("incorrect hash")
 
-func (r *UploadRequest) Validate() (error, int) {
+//rewrite validators!!!
+
+func (r *TorrentRequest) ValidateUpload() (error, int) {
 	if len(r.Name) < 100 {
 		return ErrShortName, http.StatusNotAcceptable
 	}
@@ -92,4 +100,59 @@ func (r *UploadRequest) Validate() (error, int) {
 	}
 
 	return nil, http.StatusOK
+}
+
+func (r *TorrentRequest) ValidateUpdate() (error, int) {
+	if len(r.Name) < 100 && len(r.Name) != 0 {
+		return ErrShortName, http.StatusNotAcceptable
+	}
+	/*if r.Category == 0 {
+		return ErrCategory, http.StatusNotAcceptable
+	}
+	if r.SubCategory == 0 {
+		return ErrSubCategory, http.StatusNotAcceptable
+	}*/
+
+	if r.Magnet != "" || r.Hash != "" {
+		if r.Hash == "" {
+			magnetUrl, err := url.Parse(string(r.Magnet)) //?
+			if err != nil {
+				return err, http.StatusInternalServerError
+			}
+			exactTopic := magnetUrl.Query().Get("xt")
+			if !strings.HasPrefix(exactTopic, "urn:btih:") {
+				return ErrMagnet, http.StatusNotAcceptable
+			}
+			r.Hash = strings.ToUpper(strings.TrimPrefix(exactTopic, "urn:btih:"))
+		}
+
+		matched, err := regexp.MatchString("^[0-9A-F]{40}$", r.Hash)
+		if err != nil {
+			return err, http.StatusInternalServerError
+		}
+		if !matched {
+			return ErrHash, http.StatusNotAcceptable
+		}
+	}
+
+	return nil, http.StatusOK
+}
+
+//rewrite with reflect ?
+func (r *UpdateRequest) UpdateTorrent(t *model.Torrents) {
+	if r.Update.Name != "" {
+		t.Name = r.Update.Name
+	}
+	if r.Update.Hash != "" {
+		t.Hash = r.Update.Hash
+	}
+	if r.Update.Category != 0 {
+		t.Category = r.Update.Category
+	}
+	if r.Update.SubCategory != 0 {
+		t.Sub_Category = r.Update.SubCategory
+	}
+	if r.Update.Description != "" {
+		t.Description = r.Update.Description
+	}
 }
