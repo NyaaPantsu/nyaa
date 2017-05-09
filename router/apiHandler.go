8b1c99fdad52f70447e61dbd287464f32201bcd5
ayame-git <s.varezhkin.601@gmail.com>
2017-05-09 21:54:50 +0300

    update

--- router/apiHandler.go
+++ router/apiHandler.go
@@ -94,10 +94,9 @@ func ApiUploadHandler(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Error uploads are disabled", http.StatusInternalServerError)
 		return
 	}
+
 	contentType := r.Header.Get("Content-Type")
-
 	if contentType == "application/json" {
-
 		token := r.Header.Get("Authorization")
 		user := model.User{}
 		db.ORM.Where("api_token = ?", token).First(&user) //i don't like this
@@ -108,13 +107,13 @@ func ApiUploadHandler(w http.ResponseWriter, r *http.Request) {
 
 		defer r.Body.Close()
 
-		upload := apiService.UploadRequest{}
+		upload := apiService.TorrentRequest{}
 		d := json.NewDecoder(r.Body)
 		if err := d.Decode(&upload); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-		err, code := upload.Validate()
+		err, code := upload.ValidateUpload()
 		if err != nil {
 			http.Error(w, err.Error(), code)
 			return
@@ -139,5 +138,50 @@ func ApiUploadHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func ApiUpdateHandler(w http.ResponseWriter, r *http.Request) {
+	if config.UploadsDisabled == 1 {
+		http.Error(w, "Error uploads are disabled", http.StatusInternalServerError)
+		return
+	}
 
+	contentType := r.Header.Get("Content-Type")
+	if contentType == "application/json" {
+		token := r.Header.Get("Authorization")
+		user := model.User{}
+		db.ORM.Where("api_token = ?", token).First(&user) //i don't like this
+		if user.Id == 0 {
+			http.Error(w, "incorrect api key", http.StatusForbidden)
+			return
+		}
+
+		defer r.Body.Close()
+
+		update := apiService.UpdateRequest{}
+		d := json.NewDecoder(r.Body)
+		if err := d.Decode(&update); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		id := update.Id
+		torrent := model.Torrents{}
+		db.ORM.Where("torrent_id = ?", id).First(&torrent)
+		if torrent.Id == 0 {
+			http.Error(w, "incorrect id", http.StatusBadRequest)
+			return
+		}
+		if torrent.UploaderId != 0 && torrent.UploaderId != user.Id { //&& user is not mod
+			http.Error(w, "not enough rights to edit torrent", http.StatusForbidden)
+			return
+		}
+		err, code := update.Update.ValidateUpdate()
+		if err != nil {
+			http.Error(w, err.Error(), code)
+			return
+		}
+		fmt.Printf("%+v\n", torrent)
+		update.UpdateTorrent(&torrent)
+
+		db.ORM.Save(&torrent)
+		fmt.Printf("%+v\n", torrent)
+	}
 }
