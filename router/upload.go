fd16bfd9deb987ec49717f7b0b4257162b347371
ayame-git <s.varezhkin.601@gmail.com>
2017-05-11 06:04:11 +0300

    stuff

--- router/upload.go
+++ router/upload.go
@@ -17,6 +17,7 @@ import (
 	"github.com/ewhal/nyaa/cache"
 	"github.com/ewhal/nyaa/config"
 	"github.com/ewhal/nyaa/service/captcha"
+	"github.com/ewhal/nyaa/service/upload"
 	"github.com/ewhal/nyaa/util"
 	"github.com/ewhal/nyaa/util/metainfo"
 	"github.com/microcosm-cc/bluemonday"
@@ -30,7 +31,7 @@ type UploadForm struct {
 	Category    string
 	Remake      bool
 	Description string
-	Status int
+	Status      int
 	captcha.Captcha
 
 	Infohash      string
@@ -133,7 +134,7 @@ func (f *UploadForm) ExtractInfo(r *http.Request) error {
 			return ErrPrivateTorrent
 		}
 		trackers := torrent.GetAllAnnounceURLS()
-		if !CheckTrackers(trackers) {
+		if !uploadService.CheckTrackers(trackers) {
 			return ErrTrackerProblem
 		}
 
@@ -234,35 +235,6 @@ func WriteTorrentToDisk(file multipart.File, name string, fullpath *string) erro
 	}
 	*fullpath = fmt.Sprintf("%s%c%s", config.TorrentFileStorage, os.PathSeparator, name)
 	return ioutil.WriteFile(*fullpath, b, 0644)
-}
-
-func CheckTrackers(trackers []string) bool {
-	// TODO: move to runtime configuration
-	var deadTrackers = []string{ // substring matches!
-		"://open.nyaatorrents.info:6544",
-		"://tracker.openbittorrent.com:80",
-		"://tracker.publicbt.com:80",
-		"://stats.anisource.net:2710",
-		"://exodus.desync.com",
-		"://open.demonii.com:1337",
-		"://tracker.istole.it:80",
-		"://tracker.ccc.de:80",
-		"://bt2.careland.com.cn:6969",
-		"://announce.torrentsmd.com:8080"}
-
-	var numGood int
-	for _, t := range trackers {
-		good := true
-		for _, check := range deadTrackers {
-			if strings.Contains(t, check) {
-				good = false
-			}
-		}
-		if good {
-			numGood++
-		}
-	}
-	return numGood > 0
 }
 
 // NewUploadForm creates a new upload form given parameters as list
