63844f0d4b31bf3d3c90dab99eea61722d3f8eea
akuma06 <demon.akuma06@gmail.com>
2017-05-17 23:00:50 +0200

    Fix #590

--- public/js/main.js
+++ public/js/main.js
@@ -40,34 +40,3 @@ window.onload = function() {
 if (location.hash) shiftWindow();
 window.addEventListener("hashchange", shiftWindow);
 };
-function loadLanguages() {
-	var xhr = new XMLHttpRequest();
-	xhr.onreadystatechange = function() {
-		if (xhr.readyState == 4 && xhr.status == 200) {
-			var selector = document.getElementById("bottom_language_selector");
-			selector.hidden = false
-			/* Response format is
-			 * { "current": "(user current language)",
-			 *   "languages": {
-			 *   	"(language_code)": "(language_name"),
-			 *   }} */
-			var response = JSON.parse(xhr.responseText);
-			for (var language in response.languages) {
-				if (!response.languages.hasOwnProperty(language)) continue;
-
-				var opt = document.createElement("option")
-				opt.value = language
-				opt.innerHTML = response.languages[language]
-				if (language == response.current) {
-					opt.selected = true
-				}
-
-				selector.appendChild(opt)
-			}
-		}
-	}
-	xhr.open("GET", "/language?format=json", true)
-	xhr.send()
-}
-
-loadLanguages();
