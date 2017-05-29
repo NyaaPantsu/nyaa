f7dc5b78b1317c7b8a571af59bc9899a7a2f2761
bragason <bragason@stand-mixer>
2017-05-29 18:21:44 +0000

    Reimplemented night mode and mascot hiding

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,13 @@
 var explosion = document.getElementById("explosion");
 var nyanpassu = document.getElementById("nyanpassu");
 
-function toggleNightMode() {
-	var night = localStorage.getItem("night");
-	if(night == "true") {
-		document.getElementsByTagName("head")[0].removeChild(darkStyleLink);
-	} else {
-		document.getElementsByTagName("head")[0].appendChild(darkStyleLink);
-	}
-	localStorage.setItem("night", (night == "true") ? "false" : "true");
-}
+function switchThemes(themeName=null){
+	// Switches between themes when a new one is selected
 
-// Switches between themes when a new one is selected
-function switchThemes(){
-	themeName = document.getElementById("theme-selector").value
+	// Get the theme from the selector if we are not manually passing one
+	if(!themeName){
+		themeName = document.getElementById("theme-selector").value
+	}
 	var head = document.getElementsByTagName("head")[0];
 	// Remove the theme in place, it fails if one isn't set
 	try{
@@ -31,17 +25,57 @@ function switchThemes(){
 	head.appendChild(newTheme);
 }
 
+function toggleNightMode(currentTheme){
+	// Turns night mode on and off
+	nightMode = sessionStorage.nightMode;
+	if(nightMode === undefined){
+		nightMode = "false";
+	}
+	console.log(nightMode);
+	// If nightmode is on, turn it off
+	if(nightMode === "true"){
+		console.log("turning off!")
+		sessionStorage.nightMode = "false";
+		switchThemes(sessionStorage.previousTheme);
+	}
+	// If nightmode is turned off, turn it on
+	else{
+		console.log("turning on!");
+		sessionStorage.nightMode = "true";
+		// Hardcoded to g for now, I need to find a better solution here
+		sessionStorage.setItem("previousTheme", currentTheme);
+		switchThemes("tomorrow");
+	}
+}
 
-function toggleMascot(btn) {
-	var state= btn.value;
-	if (state == "hide") {
-		btn.innerHTML = "Mascot";
+function themeFixes(){
+	// Loads night mode if it is turned on, meant to be called onload
+	// Unloads the mascot too
+	if(sessionStorage.nightMode === "true"){
+		switchThemes("tomorrow");
+	}
+	if(localStorage.fuckRenge === "true"){
 		document.getElementById("mascot").className = "hide";
-		btn.value = "show";
+	}
+}
+
+function disableNightMode(){
+	// Called when a user switches themes, we don't want him to automatically go back to nightmode
+	sessionStorage.nightMode = "false";
+}
+
+function toggleMascot() {
+	var fuckRenge = localStorage.fuckRenge;
+        if(fuckRenge === undefined){
+		fuckRenge = "false";
+	}
+	// If it is not specifically disabled, covers the undefined case as well
+	if (fuckRenge === "false") {
+		localStorage.fuckRenge = "true";
+		document.getElementById("mascot").className = "hide";
 	} else {
-		btn.innerHTML = "Mascot";
+		localStorage.fuckRenge = "false";
 		document.getElementById("mascot").className = "";
-		btn.value = "hide";
 	}
 }
 
@@ -71,12 +105,19 @@ for(var i in list) {
 	e.title = e.innerText;
 	e.innerText = new Date(e.innerText).toLocaleString(lang);
 }
+
 /*Fixed-Navbar offset fix*/
 window.onload = function() {
 	var shiftWindow = function() { scrollBy(0, -70) };
 	if (location.hash) shiftWindow();
 	window.addEventListener("hashchange", shiftWindow);
 };
+
+// $(document).ready equivilent, prevents night mode flickering
+document.addEventListener("DOMContentLoaded", function(event) { 
+	themeFixes();
+});
+
 
 function playVoice() {
 	if (explosion) {
