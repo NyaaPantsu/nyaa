ae1628e921890c4a11994079f3dc02d17503a764
akuma06 <demon.akuma06@gmail.com>
2017-05-30 15:19:45 +0200

    Some js fixes and improvements
    Better handling of Torrents.StopRefresh()
    Keep the order of torrents
    Encode html entities from torrent name (prevent execution of unwanted
    html tags)

--- public/js/torrents.js
+++ public/js/torrents.js
@@ -1,32 +1,39 @@
 var Torrents = {
     CanRefresh: false,
     timeout: undefined,
-    Seconds: 300,
+    Seconds: 300, // Every five minutes, can be overridden directly in home.html (not here is better)
     SearchURL: "/api/search",
     Method: "prepend",
     LastID: 0,
     StopRefresh: function() {
         clearTimeout(this.timeout)
         this.timeout = undefined
+        this.CanRefresh = false
+    },
+    Refresh: function() {
+        if (this.CanRefresh) {
+            console.log("Start Refresh...")
+            this.timeout = setTimeout(function() {
+                var searchArgs = (window.location.search != "") ? window.location.search.substr(1) : ""
+                searchArgs = (Torrents.LastID > 0) ? "?fromID="+Torrents.LastID+"&"+searchArgs : "?"+searchArgs
+                Query.Get(Torrents.SearchURL+searchArgs, 
+                    Templates.ApplyItemListRenderer({
+                        templateName: "torrents.item", method: "prepend", element: document.getElementById("torrentListResults")
+                    }), function(torrents) {
+                        for (var i =0; i < torrents.length; i++) { if (Torrents.LastID < torrents[i].id) Torrents.LastID = torrents[i].id; }
+                        parseAllDates();
+                        Torrents.Refresh()
+                    });
+            }, this.Seconds*1000);
+        }
     },
     StartRefresh: function() {
-        console.log("Start Refresh...")
-        this.timeout = setTimeout(function() {
-            var searchArgs = (window.location.search != "") ? window.location.search.substr(1) : ""
-            searchArgs = (Torrents.LastID > 0) ? "?fromID="+Torrents.LastID+"&"+searchArgs : "?"+searchArgs
-            Query.Get(Torrents.SearchURL+searchArgs, 
-                Templates.ApplyItemListRenderer({
-                    templateName: "torrents.item", method: "prepend", element: document.getElementById("torrentListResults")
-                }), function(torrents) {
-                    for (var i =0; i < torrents.length; i++) { if (Torrents.LastID < torrents[i].id) Torrents.LastID = torrents[i].id; }
-                    parseAllDates();
-                    Torrents.StartRefresh()
-                });
-        }, this.Seconds*1000);
+        this.CanRefresh = true;
+        this.Refresh()
     }
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() { // if Torrents.CanRefresh is enabled, refresh is automatically done (no need to start it anually)
     if (Torrents.CanRefresh) {
         Torrents.StartRefresh()
     }
