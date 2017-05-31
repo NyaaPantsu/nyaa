133947df0aa00921148c689be1d56b2459180087
akuma06 <demon.akuma06@gmail.com>
2017-05-31 12:36:39 +0200

    Updating xhr request
    * Querying when request has failed
    * Added a maximum consecutive query parameter to limit query when server
    has shutdown
    * By default I've put it to -1 (no maximum), it can be modified
    * Stopping with an error when too many failed query
    * Removing log message in torrents.js

--- public/js/query.js
+++ public/js/query.js
@@ -1,4 +1,6 @@
 var Query = {
+    Failed:0,
+    MaxConsecutingFailing:-1,
     Get: function(url, renderer, callback) {
         var xhr = new XMLHttpRequest();
         console.log(url)
@@ -6,8 +8,15 @@ var Query = {
         xhr.responseType = 'json';
         xhr.onload = function(e) {
             if (this.status == 200) {
+                Query.Failed = 0;
                 renderer(this.response);
-                callback(this.response)
+                callback(this.response);
+            } else {
+                console.log("Error when refresh")
+                Query.Failed++;
+                console.log("Attempt to refresh "+Query.Failed+"...");
+                if ((Query.MaxConsecutingFailing == -1) || (Query.Failed < Query.MaxConsecutingFailing)) Query.Get();
+                else console.error("Too many attempts, stopping...")
             }
         };
         xhr.send();
