79fbb4085d31b1d7db64034fa3421d49feee936e
Atvaark <Atvaark@users.noreply.github.com>
2017-06-04 12:52:22 +0200

    Update vendor/github.com/anacrolix (#909)
    - This fixes a bug where the torrent client didn't unlock the bolt db
      See: anacrolix/torrent/issues/158

--- vendor/github.com/anacrolix/torrent/client.go
+++ vendor/github.com/anacrolix/torrent/client.go
@@ -46,6 +46,7 @@ type Client struct {
 	halfOpenLimit  int
 	peerID         [20]byte
 	defaultStorage *storage.Client
+	onClose        []func()
 	tcpListener    net.Listener
 	utpSock        *utp.Socket
 	dHT            *dht.Server
@@ -238,6 +239,12 @@ func NewClient(cfg *Config) (cl *Client, err error) {
 		dopplegangerAddrs: make(map[string]struct{}),
 		torrents:          make(map[metainfo.Hash]*Torrent),
 	}
+	defer func() {
+		if err == nil {
+			return
+		}
+		cl.Close()
+	}()
 	if cfg.UploadRateLimiter == nil {
 		cl.uploadLimit = rate.NewLimiter(rate.Inf, 0)
 	} else {
@@ -253,6 +260,11 @@ func NewClient(cfg *Config) (cl *Client, err error) {
 	storageImpl := cfg.DefaultStorage
 	if storageImpl == nil {
 		storageImpl = storage.NewFile(cfg.DataDir)
+		cl.onClose = append(cl.onClose, func() {
+			if err := storageImpl.Close(); err != nil {
+				log.Printf("error closing default storage: %s", err)
+			}
+		})
 	}
 	cl.defaultStorage = storage.NewClient(storageImpl)
 	if cfg.IPBlocklist != nil {
@@ -336,6 +348,9 @@ func (cl *Client) Close() {
 	}
 	for _, t := range cl.torrents {
 		t.close()
+	}
+	for _, f := range cl.onClose {
+		f()
 	}
 	cl.event.Broadcast()
 }
@@ -899,7 +914,7 @@ func (cl *Client) runInitiatedHandshookConn(c *connection, t *Torrent) {
 		cl.dopplegangerAddrs[addr] = struct{}{}
 		return
 	}
-	cl.runHandshookConn(c, t)
+	cl.runHandshookConn(c, t, true)
 }
 
 func (cl *Client) runReceivedConn(c *connection) {
@@ -926,14 +941,14 @@ func (cl *Client) runReceivedConn(c *connection) {
 		// doppleganger.
 		return
 	}
-	cl.runHandshookConn(c, t)
+	cl.runHandshookConn(c, t, false)
 }
 
-func (cl *Client) runHandshookConn(c *connection, t *Torrent) {
+func (cl *Client) runHandshookConn(c *connection, t *Torrent, outgoing bool) {
 	c.conn.SetWriteDeadline(time.Time{})
 	c.r = deadlineReader{c.conn, c.r}
 	completedHandshakeConnectionFlags.Add(c.connectionFlags(), 1)
-	if !t.addConnection(c) {
+	if !t.addConnection(c, outgoing) {
 		return
 	}
 	defer t.dropConnection(c)
