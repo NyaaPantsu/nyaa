d84c294c1bfd7d80a6f627c3463df7d5a5bc0c39
akuma06 <demon.akuma06@gmail.com>
2017-07-03 01:47:31 +0200

    Binding requirements for gin

--- utils/validator/torrent/forms.go
+++ utils/validator/torrent/forms.go
@@ -3,21 +3,21 @@ package torrentValidator
 // TorrentRequest struct
 // Same json name as the constant!
 type TorrentRequest struct {
-	Name        string   `validate:"required" json:"name,omitempty"`
-	Magnet      string   `json:"magnet,omitempty"`
-	Category    string   `validate:"required" json:"c"`
-	Remake      bool     `json:"remake,omitempty"`
-	Description string   `json:"desc,omitempty"`
-	Status      int      `json:"status,omitempty"`
-	Hidden      bool     `json:"hidden,omitempty"`
-	CaptchaID   string   `json:"-"`
-	WebsiteLink string   `validate:"uri" json:"website_link,omitempty"`
-	SubCategory int      `json:"sub_category,omitempty"`
-	Languages   []string `json:"languages,omitempty"`
+	Name        string   `validate:"required" form:"name" json:"name,omitempty"`
+	Magnet      string   `json:"magnet,omitempty" form:"magnet"`
+	Category    string   `validate:"required" form:"c" json:"c"`
+	Remake      bool     `json:"remake,omitempty" form:"remake"`
+	Description string   `json:"desc,omitempty" form:"desc"`
+	Status      int      `json:"status,omitempty" form:"status"`
+	Hidden      bool     `json:"hidden,omitempty" form:"hidden"`
+	CaptchaID   string   `json:"-" form:"captchaID"`
+	WebsiteLink string   `validate:"uri" json:"website_link,omitempty" form:"website_link"`
+	SubCategory int      `json:"sub_category,omitempty" form:"sub_category"`
+	Languages   []string `json:"languages,omitempty" form:"languages"`
 
-	Infohash      string         `json:"hash,omitempty"`
-	CategoryID    int            `json:"-"`
-	SubCategoryID int            `json:"-"`
+	Infohash      string         `json:"hash,omitempty" form:"hash"`
+	CategoryID    int            `json:"-" form:"category_id"`
+	SubCategoryID int            `json:"-" form:"subcategory_id"`
 	Filesize      int64          `json:"filesize,omitempty"`
 	Filepath      string         `json:"-"`
 	FileList      []uploadedFile `json:"filelist,omitempty"`
