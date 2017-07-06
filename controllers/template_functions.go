c2fe8aa99e4c9544d1f9d5d5e6f62fe745e38069
PantsuDev <pantsudev@local>
2017-07-06 11:56:52 +1000

    Fix /search/search bug

--- controllers/template_functions.go
+++ controllers/template_functions.go
@@ -118,7 +118,7 @@ func templateFunctions(vars jet.VarMap) jet.VarMap {
 			}
 			for i := startValue; i <= endValue; i++ {
 				pageNum := strconv.Itoa(i)
-				url := nav.Route + "/" + pageNum
+				url := "/search/" + pageNum
 				ret = ret + "<a aria-label=\"Page " + strconv.Itoa(i) + "\" href=\"" + url + "?" + currentUrl.RawQuery + "\">" + "<li"
 				if i == nav.CurrentPage {
 					ret = ret + " class=\"active\""
