7382f175b959400c1e075f31887c89f8863365e2
kilo <kilowwwww@gmail.com>
2017-07-14 21:34:25 +0200

    JS script, missing flag stuff

---
+++ public/js/kilo.js
@@ -0,0 +1,28 @@
+document.getElementsByClassName("form-torrent-name")[0].onkeyup = function(){
+    document.getElementsByClassName("table-torrent-name")[0].innerText = document.getElementsByClassName("form-torrent-name")[0].value;
+};
+
+document.getElementsByClassName("form-torrent-category")[0].onchange = function(){
+    document.getElementsByClassName("table-torrent-category")[0].className = "table-torrent-category nyaa-cat nyaa-cat-" + Categorylist[document.getElementsByClassName("form-torrent-category")[0].selectedIndex];
+};
+
+var CategoryList = [
+    5
+    12,
+    5,
+    13,
+    6,
+    3,
+    4,
+    7,
+    14,
+    8,
+    9,
+    10,
+    18,
+    11,
+    15,
+    16,
+    1,
+    2
+];
