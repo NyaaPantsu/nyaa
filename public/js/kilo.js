7415843336aab6e01feca9075807ca1002e5d821
kilo <kilowwwww@gmail.com>
2017-07-14 22:21:12 +0200

    Update kilo.js

--- public/js/kilo.js
+++ public/js/kilo.js
@@ -7,8 +7,14 @@ document.getElementsByClassName("form-torrent-category")[0].onchange = function(
 };
 
 document.getElementsByClassName("form-torrent-remake")[0].onchange = function(){
-    document.getElementsByClassName("table-torrent-category")[0].className = "table-torrent-category nyaa-cat nyaa-cat-" + Categorylist[document.getElementsByClassName("form-torrent-category")[0].selectedIndex] + (document.getElementsByClassName("torrent-form-remake")[0].value == "on" ? "remake" : "");
+    document.getElementsByClassName("table-torrent-thead")[0].className = "torrent-info table-torrent-thead" + (UserTrusted ? " trusted : "") + (document.getElementsByClassName("form-torrent-remake")[0].value == "on" ? " remake" : "");
 };
+                                                                                                                
+document.getElementsByClassName("form-torrent-hidden")[0].onchange = function(){
+    document.getElementsByClassName("table-torrent-thead")[0].className = "torrent-info table-torrent-thead" + (document.getElementsByClassName("form-torrent-remake")[0].value == "on" ? " remake" : "");
+    if(UserTrusted && document.getElementsByClassName("form-torrent-hidden")[0].value == "off")
+        document.getElementsByClassName("table-torrent-thead")[0].className = document.getElementsByClassName("table-torrent-thead")[0].className + " trusted";
+};                                                                                                            
 
 document.getElementsByClassName("torrent-preview-table")[0].display = "block";
 
