4826835f03dde0de376879776954b5755c87c223
kilo <kilowwwww@gmail.com>
2017-07-23 16:53:59 +0200

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -54,31 +54,23 @@ function parseAllDates() {
 parseAllDates()
 
 
-  //if no version cookie set or non-equal version
-  if(!document.cookie.includes("version") || (document.cookie.substring(document.cookie.indexOf("version") + 8).substring(0, document.cookie.substring(document.cookie.indexOf("version") + 8).indexOf(";") == "-1" ? document.cookie.substring(document.cookie.indexOf("version") + 8).length : document.cookie.substring(document.cookie.indexOf("version") + 8).indexOf(";"))) != Version)
+  //called if no Commit cookie is set or if the website has a newer commit than the one in cookie
+  function resetCookies()
   {
-	  	//Get current lang, mascot & theme cookies
-		//TODO
+    var cookies = document.cookie.split(";");
+    var excludedCookies = ["mascot", "theme", "mascot_url", "lang"];
 	  
-	 	 //Remove all cookies:
-		var cookies = document.cookie.split(";");
-	  	var excludedCookies = ["mascot", "theme", "mascot_url", "lang"];
+    //Remove all cookies but exclude those in the above array
+    for (var i = 0; i < cookies.length; i++) {
+      var cookieName = (cookies[i].split("=")[0]).trim();
+      //Remove spaces because some cookie names have it
+      if(excludedCookies.includes(cookieName)) continue;
+      document.cookie = cookieName + "=;expires=Thu, 01 Jan 1970 00:00:00 UTC;";
+    }
 	  
-		for (var i = 0; i < cookies.length; i++) {
-		   var cookieName = (cookies[i].split("=")[0]).trim();
-		   //Remove spaces because some cookie names have it
-		   if(excludedCookies.includes(cookieName)) continue;
-		   document.cookie = cookieName + "=;expires=Thu, 01 Jan 1970 00:00:00 UTC;";
-		}
-	  
-		//Set new version in cookie
-		document.cookie = "version=" + Version;
-	  
-	  	//Apply back lang, mascot & theme cookie
-	  	//TODO
-	  
-	  	//Add fancy "new" text at bottom of page
-	  	document.getElementById("commit").className = "new";
+	//Set new version in cookie and add fancy "new" text at bottom of pag
+    document.cookie = "version=" + Version;
+    document.getElementById("commit").className = "new";
   }
 		
 
@@ -94,6 +86,18 @@ function startupCode() {
   if (location.hash) shiftWindow()
   window.addEventListener("hashchange", shiftWindow)
 
+  if(!document.cookie.includes("version"))
+    resetCookies()
+  else {
+    var startPos           = document.cookie.indexOf("version") + 8,
+	endPos             = document.cookie.substring(startPos).indexOf(";"),
+	userCommitVersion  = endPos == "-1" ? document.cookie.substring(startPos) : document.cookie.substring(startPos, endPos + startPos);
+	//Get start and end position of Commit string, need to start searching endPos from version cookie in case it's not the first cookie in the string
+	//If endPos is equal to -1, aka if the version cookie is at the very end of the string and doesn't have an ";", the endPos is not used
+	if(userCommitVersion != commitVersion)
+	  resetCookies()
+  }
+	
   document.getElementsByClassName("search-box")[0].addEventListener("focus", function (e) {
     var w = document.getElementsByClassName("h-user")[0].offsetWidth
     document.getElementsByClassName("h-user")[0].style.display = "none"
