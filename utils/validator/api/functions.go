53406b463688ab9b1deec8cc0d91ee45954e35d7
akuma06 <demon.akuma06@gmail.com>
2017-07-26 23:37:12 +0200

    Sanitize empty strig in form array + css
    Multiple empty array of strings are sanitized for the oauth client create form
    Added some css for the form display

--- utils/validator/api/functions.go
+++ utils/validator/api/functions.go
@@ -4,20 +4,21 @@ import (
 	"strings"
 
 	"github.com/NyaaPantsu/nyaa/models"
+	"github.com/NyaaPantsu/nyaa/utils/sanitize"
 )
 
 func (form *CreateForm) Bind(d *models.OauthClient) *models.OauthClient {
 	d.Name = form.Name
-	d.RedirectURIs = strings.Join(form.RedirectURI, "$!$")
-	d.GrantTypes = strings.Join(form.GrantTypes, "$!$")
-	d.ResponseTypes = strings.Join(form.ResponseTypes, "$!$")
+	d.RedirectURIs = strings.Join(sanitize.ClearEmpty(form.RedirectURI), "|")
+	d.GrantTypes = strings.Join(sanitize.ClearEmpty(form.GrantTypes), "|")
+	d.ResponseTypes = strings.Join(sanitize.ClearEmpty(form.ResponseTypes), "|")
 	d.Scope = form.Scope
 	d.Owner = form.Owner
 	d.PolicyURI = form.PolicyURI
 	d.TermsOfServiceURI = form.TermsOfServiceURI
 	d.ClientURI = form.ClientURI
 	d.LogoURI = form.LogoURI
-	d.Contacts = strings.Join(form.Contacts, "$!$")
+	d.Contacts = strings.Join(sanitize.ClearEmpty(form.Contacts), "|")
 	d.Secret = form.Secret
 	return d
 }
