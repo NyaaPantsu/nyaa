e5c115b4003d804c56675672b2e504cced0459af
akuma06 <demon.akuma06@gmail.com>
2017-07-28 20:43:22 +0200

    Adding tag post controller

--- controllers/torrent/view.go
+++ controllers/torrent/view.go
@@ -20,35 +20,57 @@ func ViewHandler(c *gin.Context) {
 	messages := msg.GetMessages(c)
 	user := router.GetUser(c)
 
+	// Display success message on upload
 	if c.Request.URL.Query()["success"] != nil {
 		messages.AddInfoT("infos", "torrent_uploaded")
 	}
+	// Display success message on edit
 	if c.Request.URL.Query()["success_edit"] != nil {
 		messages.AddInfoT("infos", "torrent_updated")
 	}
+	// Display wrong captcha error message
 	if c.Request.URL.Query()["badcaptcha"] != nil {
 		messages.AddErrorT("errors", "bad_captcha")
 	}
+	// Display reported successful message
 	if c.Request.URL.Query()["reported"] != nil {
 		messages.AddInfoTf("infos", "report_msg", id)
 	}
 
+	// Retrieve the torrent
 	torrent, err := torrents.FindByID(uint(id))
 
-	if c.Request.URL.Query()["notif"] != nil {
+	// If come from notification, toggle the notification as read
+	if c.Request.URL.Query()["notif"] != nil && user.ID > 0 {
 		notifications.ToggleReadNotification(torrent.Identifier(), user.ID)
 	}
 
+	// If torrent not found, display 404
 	if err != nil {
 		c.Status(http.StatusNotFound)
 		return
 	}
+
+	// We load tags for user and torrents
+	user.LoadTags(torrent)
+	torrent.LoadTags()
+
+	// We add a tag if posted
+	if c.PostForm("tag") != "" && user.ID > 0 {
+		postTag(c, torrent, user)
+	}
+
+	// Convert torrent to the JSON Model used to display a torrent
+	// Since many datas need to be parsed from a simple torrent model to the actual display
 	b := torrent.ToJSON()
+	// Get the folder root for the filelist view
 	folder := filelist.FileListToFolder(torrent.FileList, "root")
 	captchaID := ""
+	//Generate a captcha
 	if user.NeedsCaptcha() {
 		captchaID = captcha.GetID()
 	}
+	// Display finally the view
 	templates.Torrent(c, b, folder, captchaID)
 }
 
--- models/tag.go
+++ models/tag.go
@@ -1,5 +1,12 @@
 package models
 
+import (
+	"errors"
+	"net/http"
+
+	"github.com/fatih/structs"
+)
+
 // Tag model for a torrent vote system
 type Tag struct {
 	TorrentID uint    `gorm:"column:torrent_id"`
@@ -8,4 +15,27 @@ type Tag struct {
 	Type      string  `gorm:"column:type"`
 	Weight    float64 `gorm:"column:weight"`
 	Accepted  bool    `gorm:"column:accepted"`
+	Total     float64 `gorm:"-"`
+}
+
+// Update a tag
+func (ta *Tag) Update() (int, error) {
+	if ORM.Model(ta).UpdateColumn(ta.toMap()).Error != nil {
+		return http.StatusInternalServerError, errors.New("Tag was not updated")
+	}
+	return http.StatusOK, nil
+}
+
+// Delete : delete a tag based on id
+func (ta *Tag) Delete() (int, error) {
+	if ORM.Delete(ta).Error != nil {
+		return http.StatusInternalServerError, errors.New("tag_not_deleted")
+	}
+
+	return http.StatusOK, nil
+}
+
+// toMap : convert the model to a map of interface
+func (ta *Tag) toMap() map[string]interface{} {
+	return structs.Map(ta)
 }
