37d9b8908e47f6d51e48e2dea8b0e8de82a9d373
kilo <kilowwwww@gmail.com>
2017-08-02 15:48:58 +0200

    cursor: pointer

--- public/js/main.js
+++ public/js/main.js
@@ -125,7 +125,7 @@ function startupCode() {
   if (document.cookie.includes("newVersion"))
     document.getElementById("commit").className = document.getElementById("commit").innerHTML != "unknown" ? "new" : "wew";
 
-  document.getElementById("dark-toggle").style.display = "initial";
+  document.getElementById("dark-toggle").style = "display: initial; cursor: pointer;"
   document.getElementById("dark-toggle").addEventListener("click", toggleTheme);
 
   if(document.cookie.includes("theme")) {
