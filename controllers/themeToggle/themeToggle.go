1acbc5341515457e00f16f1c04b229cf5ddad7a2
kilo <kilowwwww@gmail.com>
2017-08-20 16:27:53 +0200

    Rollback to fix errors

--- controllers/themeToggle/themeToggle.go
+++ controllers/themeToggle/themeToggle.go
@@ -2,6 +2,7 @@ package themeToggleController
 
 import (
 	"net/http"
+	"log"
 
 	"github.com/NyaaPantsu/nyaa/config"
 	"github.com/NyaaPantsu/nyaa/utils/timeHelper"
@@ -20,27 +21,14 @@ func toggleThemeHandler(c *gin.Context) {
 	if err != nil {
 		theme2 = "tomorrow"
 	}
-	if theme == theme2 {
-		if theme == "tomorrow" {
-			theme2 = "g"
-		}
-		if theme != "tomorrow" {
-			theme2 = "tomorrow"	
-		}
-	}
-	//Get theme & theme2 value, if not set by default is g.css & tomorrow.css
-	//if both theme are identical, which can happen, we fix it
-	
-	//Set value of redirect url, add #footer to get user back to same page position
-	redirectUrl = "https://nyaa.pantsu.cat/"
-	fmt.Sprintf(redirectUrl, "%s#footer", redirectUrl)
+	//Get theme1 & theme2 value, set g.css & tomorrow.css by default
 
-	//Switch theme & thele2 value
+	//Switch theme & theme2 value
 	http.SetCookie(c.Writer, &http.Cookie{Name: "theme", Value: theme2, Domain: getDomainName(), Path: "/", Expires: timeHelper.FewDaysLater(365)})
 	http.SetCookie(c.Writer, &http.Cookie{Name: "theme2", Value: theme, Domain: getDomainName(), Path: "/", Expires: timeHelper.FewDaysLater(365)})	
 	
-	//redirect user to page
-	c.Redirect(http.StatusSeeOther, redirectUrl)
+	//redirect user to page he was beforehand
+	c.Redirect(http.StatusSeeOther, c.Param("redirect"))
 	return
 }
 
