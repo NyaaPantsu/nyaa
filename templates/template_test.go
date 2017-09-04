c2bcdd405971e82a15db4a7c4370135f0ed0ce7e
kilo <kilowwwww@gmail.com>
2017-09-04 04:52:37 +0200

    change var name

--- templates/template_test.go
+++ templates/template_test.go
@@ -284,7 +284,7 @@ func mockupCommonvariables(t *testing.T) jet.VarMap {
 	variables.Set("T", T)
 	variables.Set("Theme", "test")
 	variables.Set("AltColors", "test")
-	variables.Set("AdsEnabled", "test")
+	variables.Set("HideAds", "test")
 	variables.Set("Mascot", "test")
 	variables.Set("MascotURL", "test")
 	variables.Set("User", &models.User{})
