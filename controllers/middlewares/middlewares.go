1db6bbf32ae03b98dbc6a2f5010a157be2b79636
kilo <kilowwwww@gmail.com>
2017-09-05 15:30:37 +0200

    remove useless thing

--- controllers/middlewares/middlewares.go
+++ controllers/middlewares/middlewares.go
@@ -75,7 +75,7 @@ func ScopesRequired(scopes ...string) gin.HandlerFunc {
 // CSP set Content Security Policy http header
 func CSP() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Header("Content-Security-Policy", "default-src 'self'; img-src * data:; media-src *; style-src 'self' maxcdn.bootstrapcdn.com fonts.googleapis.com 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval' ad.a-ads.com a-ads.com *.a-ads.com; font-src 'self' fonts.gstatic.com maxcdn.bootstrapcdn.com; child-src ad.a-ads.com a-ads.com *.a-ads.com")
+		c.Header("Content-Security-Policy", "default-src 'self'; img-src * data:; media-src *; style-src 'self' maxcdn.bootstrapcdn.com fonts.googleapis.com 'unsafe-inline'; script-src 'self' 'unsafe-inline' 'unsafe-eval' a-ads.com *.a-ads.com; font-src 'self' fonts.gstatic.com maxcdn.bootstrapcdn.com; child-src ad.a-ads.com a-ads.com *.a-ads.com")
 		c.Next()
 	}
 }
