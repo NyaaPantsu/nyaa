40847a28090b2950858909b3967ef2f21aa91525
kilo <kilowwwww@gmail.com>
2017-09-05 09:28:12 +0200

    Make download.go redirect to file missing page instead of 404

--- controllers/torrent/download.go
+++ controllers/torrent/download.go
@@ -19,7 +19,7 @@ func DownloadTorrent(c *gin.Context) {
 
 	if hash == "" && len(config.Get().Torrents.FileStorage) == 0 {
 		//File not found, send 404
-		c.AbortWithError(http.StatusNotFound, errors.New("File not found"))
+		templates.Render(c, "errors/torrent_file_missing.jet.html", variables)
 		return
 	}
 
@@ -27,7 +27,7 @@ func DownloadTorrent(c *gin.Context) {
 	Openfile, err := os.Open(fmt.Sprintf("%s%c%s.torrent", config.Get().Torrents.FileStorage, os.PathSeparator, hash))
 	if err != nil {
 		//File not found, send 404
-		c.AbortWithError(http.StatusNotFound, errors.New("File not found"))
+		templates.Render(c, "errors/torrent_file_missing.jet.html", variables)
 		return
 	}
 	defer Openfile.Close() //Close after function return
@@ -40,7 +40,7 @@ func DownloadTorrent(c *gin.Context) {
 
 	if err != nil {
 		//File not found, send 404
-		c.AbortWithError(http.StatusNotFound, errors.New("File not found"))
+		templates.Render(c, "errors/torrent_file_missing.jet.html", variables)
 		return
 	}
 
