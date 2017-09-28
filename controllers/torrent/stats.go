c3ce49dc558759b85ae3cc6713f8b43492fa8d87
kilo <kilowwwww@gmail.com>
2017-09-28 14:04:00 +0200

    Create stats.go

---
+++ controllers/torrent/stats.go
@@ -0,0 +1,37 @@
+package torrentController
+
+import (
+	"net/http"
+	"strconv"
+	"fmt"
+
+	"github.com/NyaaPantsu/nyaa/models/torrents"
+	"github.com/gin-gonic/gin"
+	"text/template"
+)
+
+// ViewHeadHandler : Controller for getting torrent stats
+func GetStatsHandler(c *gin.Context) {
+	id, err := strconv.ParseInt(c.Param("id"), 10, 32)
+	if err != nil {
+		return
+	}
+
+	_, err = torrents.FindRawByID(uint(id))
+
+	if err != nil {
+		c.Status(http.StatusNotFound)
+		return
+	}
+	
+	seeders := 3
+	leechers := 3
+	downloads := 3
+  //TODO: fetch torrent stats and store it in the above variables 
+	
+	t, err := template.New("foo").Parse(fmt.Sprintf(`{{define "stats"}}{ "seeders":[%d], "leechers": [%d], "downloads": [%d] }{{end}}`, seeders, leechers, downloads))
+	err = t.ExecuteTemplate(c.Writer, "stats", "")
+	
+	return
+}
+
