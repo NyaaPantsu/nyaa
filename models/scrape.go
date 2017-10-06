cd7bfe444783c9bb8c74b4755c46dc333cd6638a
kilo <kilowwwww@gmail.com>
2017-10-06 13:25:04 +0200

    Update scrape.go

--- models/scrape.go
+++ models/scrape.go
@@ -1,9 +1,15 @@
 package models
 
 import (
+	"net/http"
+	"errors"
 	"time"
+	"fmt"
 
 	"github.com/NyaaPantsu/nyaa/config"
+	"github.com/NyaaPantsu/nyaa/utils/cache"
+	"github.com/NyaaPantsu/nyaa/utils/log"
+	"github.com/fatih/structs"
 )
 
 // Scrape model
@@ -18,4 +24,52 @@ type Scrape struct {
 // TableName : return the table name of the scrape table
 func (t Scrape) TableName() string {
 	return config.Get().Models.ScrapeTableName
+}
+
+// Update : Update scrape data based on Scrape model
+func (s *Scrape) Update(unscope bool) (int, error) {
+	db := ORM
+	if unscope {
+		db = ORM.Unscoped()
+	}
+
+	if db.Model(s).UpdateColumn(s.toMap()).Error != nil {
+		return http.StatusInternalServerError, errors.New("Scrape data was not updated")
+	}
+
+	// We only flush cache after update
+	cache.C.Delete(s.Identifier())
+
+	return http.StatusOK, nil
+}
+
+
+// toMap : convert the model to a map of interface
+func (s *Scrape) toMap() map[string]interface{} {
+	return structs.Map(s)
+}
+
+// Identifier : Return the identifier of a torrent
+func (s *Scrape) Identifier() string {
+	return fmt.Sprintf("torrent_%d", s.TorrentID)
+}
+
+//Create a Scrape entry in the DB
+func CreateScrapeData(torrentid uint, seeders uint32, leechers uint32, completed uint32, lastscrape time.Time) (*Scrape) {
+	ScrapeData := Scrape{
+		TorrentID:      torrentid,
+		Seeders:    	seeders,
+		Leechers: 		leechers,
+		Completed:      completed,
+		LastScrape:     lastscrape}
+
+	err := ORM.Create(&ScrapeData).Error
+	log.Infof("Scrape data ID %d created!\n", ScrapeData.TorrentID)
+	if err != nil {
+		log.CheckErrorWithMessage(err, "ERROR_SCRAPE_CREATE: Cannot create a scrape data")
+	}
+
+	ScrapeData.Update(false)
+
+	return &ScrapeData
 }
