c66240cdec5658786737b44483a7288688079c47
kilo <kilowwwww@gmail.com>
2017-10-28 00:23:36 +0200

    Update user.go

--- models/user.go
+++ models/user.go
@@ -180,11 +180,11 @@ func (u *User) NeedsCaptcha() bool {
 
 // CanUpload :  Check if a user can upload  or if upload is enabled in config
 func (u *User) CanUpload() bool {
-	if config.Get().Torrents.UploadsDisabled {
-		if config.Get().Torrents.AdminsAreStillAllowedTo && u.IsModerator() {
+	if config.Get().Upload.UploadsDisabled {
+		if config.Get().Upload.AdminsAreStillAllowedTo && u.IsModerator() {
 			return true
 		}
-		if config.Get().Torrents.TrustedUsersAreStillAllowedTo && u.IsTrusted() {
+		if config.Get().Upload.TrustedUsersAreStillAllowedTo && u.IsTrusted() {
 			return true
 		}
 		return false
